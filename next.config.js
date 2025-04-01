d5b23dceb90a4f893d6aa05b7662b8596be63578
Abdallayasseer <2302939@student.eelu.edu.eg>
2025-04-01 16:10:00 +0200

    Fix: Update next.config.js for image domains

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['fakestoreapi.com'], // Updated from remotePatterns
-  },
-};
-
-module.exports = nextConfig;
